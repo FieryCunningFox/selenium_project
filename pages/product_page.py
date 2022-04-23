a1330a8de39f44902ae55f09f8ba71b36a60b2df
Svetlana <svetl.rudnewa2014@yandex.ru>
2022-04-23 20:22:20 +0300

    new test for product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -3,13 +3,14 @@ from selenium.common.exceptions import NoSuchElementException
 from .locators import ProductPageLocators
 
 
-class ProductPage(BasePage): 
-    def should_be_added_to_basket(self):
+class ProductPage(BasePage):
+    def push_button(self):
         try:
             self.browser.find_element(*ProductPageLocators.BUTTON_ADD).click()
         except NoSuchElementException:
             return "Button added product to busket not found"
         
+    def should_be_added_to_basket(self):
         self.solve_quiz_and_get_code()
         self.should_price_basket_be_similar_to_product()
         self.should_name_in_message_be_similar_to_product()
@@ -27,3 +28,11 @@ class ProductPage(BasePage):
         message_expected = f"{name} has been added to your basket."
         print("Product name: ", name, "Message: ", message)
         assert message_expected == message, "Name in message is wrong"
+        
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), "Success message is presented, but should not be"
+        
+    def should_not_be_success_message_2(self):
+        result = self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE)
+        assert result == True, "Success message is presented, but should not be"
+    
